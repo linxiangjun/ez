adcd675e60d09399eb07c8a54d50804395b80701
LXJ <linxiangjun@movee.cn>
2017-04-21 15:16:46 +0800

    add ez.js

--- README.md
+++ README.md
@@ -1,2 +1,106 @@
-# ez
-实用、小巧、简洁的JS函数库 —— 提升开发效率
+#Ez.js
+
+为了提高开发的效率，同时增强各种容错率，开发出了Ez.js函数库。解决了一些开发环境中常见的业务需求，节省了开发的时间。ez.js不依赖任何框架或插件，只需要在script中引用外部文件就可以使用。
+
+##使用方法
+
+**在body中引用**
+
+    <script src="ez.js"></srcipt>
+
+**或在head中引用**
+
+    <script src="ez.js" async></srcipt>
+
+##文档
+
+**1、将数组按属性分类**
+
+这一功能经常会在分类商品、或者分类属性中使用，返回值为分组后的对象
+
+    ez.groupBy(array, attributes)
+	
+	var arr = [{id: 1, price: 12}, {id: 2, price: 10}, {id: 3, price: 12}];
+
+	ez.grounpBy(arr, 'price');  //按价格分类
+	
+	return {[{id: 2, price: 10}], [{id: 1, price: 12}, {id: 3, price: 12}]}  //返回一个对象
+
+
+**2、升序/降序数组**
+
+这个功能很常见吧，不过现在只针对简单的数组和值进行排序
+
+    ez.orderBy(array, action)
+
+	var simplyArr = [6, 9 ,3];
+
+	ez.orderBy(simplyArr, 'asc');  //升序
+	
+	ez.orderBy(SIMPLYaRR, 'desc');  //降序
+
+**3、转换成金额**
+
+传递一个数字，自动转换为带两位数的金额
+
+    ez.toPriceFormat(number)
+
+	ez.toPriceFormat(12);
+    
+    12.00  //返回内容
+
+**4、处理状态值**
+
+一千个程序员，有一千个返回值，不过现在一个函数就可以免去这些烦恼
+
+    ez.status(ret)
+
+	true  //返回true则表示回调成功
+	false  //返回false则表示回调失败
+	
+
+**5、验证手机号码/身份证号码**
+
+一个函数解决的事情，绝不多写
+
+    ez.isMobile(phone);
+
+	ez.isIdCard(id);
+	
+
+**6、存储、获取、删除Cookie**
+
+存储		
+
+    ez.setCookie(name, value, date)
+
+	ez.setCookie('id', '1', 10);  //存储id，值为1，保存10天（默认30天）
+
+获取
+
+    ez.getCookie(name)
+
+删除
+
+	ez.delCookie(name)
+
+
+**7、存储、获取、删除localStorage**
+
+存储
+
+    ez.setLocalStorage(name, value)
+	
+	ez.setLocalStorage('id', 1)  //存储id，值为1，保存本地缓存
+
+获取
+
+	ez.getLocalStorage(name)
+
+删除
+
+	ez.removeLocalStorage(name)
+
+清除
+
+	ez.clearLocalStorage()
---
+++ ez.js
@@ -0,0 +1,230 @@
+/*
+  Ez.js library
+  Released under the MIT license
+
+  author: LXJ
+  date: 2017.4.19
+*/
+
+(function(window, undefined) {
+
+  "use strict";
+
+  var v = Object.create(null);
+
+  var mobileReg = new RegExp('^(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$');
+  var idCardReg = new RegExp('^([0-9]{17})[X|x|0-9]{1}$');
+
+  function isNumber(num) {
+    if(!isNaN(num) && num.__proto__ === Number.prototype) {
+      return num;
+    } else {
+      return false;
+    }
+  }
+
+  function isArray(arr) {
+    if(arr instanceof Array && arr.constructor === Array) {
+      if(arr.length !== 0) {
+        return arr;
+      } else {
+        return null;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  function isObject(obj) {
+    if(obj instanceof Object && obj.constructor === Object) {
+      if(JSON.stringify(obj) !== '{}') {
+        return obj;
+      } else {
+        return null;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  //Create a object array
+  var createNewArray = function(obj, item, val) {
+    obj[val] = [];
+    obj[val].unshift(item);
+  };
+
+  //Array Grouping by whatever
+  v.groupBy = function(arr, name) {
+    var obj = {};
+
+    if(isArray(arr)) {
+      arr.map(function(item) {
+        var val = item[name];
+
+        obj[val] === undefined
+          ? createNewArray(obj, item, val)
+          : obj[val].push(item);
+      })
+    } else {
+      return arr;
+    }
+    return obj;
+  };
+
+  //Quick sort arithmetic
+  function quickSort(arr) {
+    if(arr.length <= 1) {return arr}
+
+    var pivotIndex = Math.floor(arr.length / 2);
+
+    var pivot = arr.splice(pivotIndex, 1)[0];
+
+    var left = [],
+        right = [];
+
+    for(var i = 0; i < arr.length; i++) {
+      if(arr[i] < pivot) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
+    }
+    i = null;
+
+    return quickSort(left).concat([pivot], quickSort(right));
+  }
+
+  //Determine whatever the array is sort
+  v.isSort = function(arr) {
+    if(isArray(arr)) {
+      if(arr[0] < arr[1]) {
+        for(var i = 2; i < arr.length; i++) {
+          if(arr[i - 1] > arr[i]) {
+            return false;
+          } else if (i === arr.length - 1) {
+            return 'asc';
+          }
+        }
+      } else {
+        for(var i = 2; i < arr.length; i++) {
+          if(arr[i - 1] < arr[i]) {
+            return false;
+          } else if (i === arr.length - 1) {
+            return 'desc';
+          }
+        }
+      }
+    } else {
+      return arr;
+    }
+  };
+
+  //Ascending array or Descending array
+  v.orderBy = function(arr, act) {
+    if(isArray(arr)) {
+      if (!!act.match(/(asc)|(ASC)/)) {
+
+        return quickSort(arr);
+      } else if(!!act.match(/(desc)|(DESC)/)) {
+        return quickSort(arr).reverse();
+      }
+      console.warn("Please input asc or desc");
+    } else {
+      return arr;
+    }
+  };
+
+  //Transfer amount to price format
+  v.toPriceFormat = function(num) {
+    if(!isNaN(num)) {
+      return Number(num).toFixed(2);
+    } else {
+      return num;
+    }
+  };
+
+  //Solve thousands of values
+  v.status = function(ret) {
+    var arr = ['status', 'code'];  //input a response stauts
+    var reg = new RegExp("([0]|success|true)", "gi");  //input a RegExp response status's value
+
+    for(var i = 0; i < arr.length; i++) {
+      if(ret.hasOwnProperty(arr[i])) {
+        ret[arr[i]] = ret[arr[i]] + '';
+        return !!ret[arr[i]].match(reg);
+      } else if(i === arr.length - 1) {
+        console.warn("Please write a new status");
+      }
+    }
+    i = null;
+  };
+
+  v.isMobile = function(phone) {
+      return !!phone.toString().match(mobileReg);
+  };
+
+  v.isIdCard = function(id) {
+      return !!id.toString().match(idCardReg);
+  };
+
+  //Set cookie
+  v.setCookie = function(name, value, days) {
+    var days = days || 30;
+    var date = new Date();
+    date.setTime(date.getTime() + days*24*60*60*1000);
+    document.cookie = name + "="+ escape (value) + ";expires=" + date.toGMTString();
+  };
+
+  //Get cookie
+  v.getCookie = function(name) {
+    var arr, reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+    if(arr=document.cookie.match(reg)) {
+      return unescape(arr[2]);
+    } else {
+      return null;
+    }
+  };
+
+  //Delete cookie
+  v.delCookie = function(name) {
+    var date = new Date();
+    date.setTime(date.getTime() - 1);
+    var cval = v.getCookie(name);
+    if(cval !== null) {
+      document.cookie= name + "=" + cval + ";expires=" + date.toGMTString();
+    }
+  };
+
+  //set localStorage
+  v.setLocalStorage = function(name, value) {
+    var val = JSON.stringify(value);
+    window.localStorage.setItem(name, val);
+  };
+
+  v.getLocalStorage = function(name) {
+    return JSON.parse(window.localStorage.getItem(name));
+  };
+
+  v.removeLocalStorage = function(name) {
+    window.localStorage.removeItem(name);
+  };
+
+  v.clearLocalStorage = function() {
+    window.localStorage.clear();
+  };
+
+  //Get URL delivery param
+  v.getURLParam = function(name) {
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if(r !== null) {
+      return  unescape(r[2]);
+      return null;
+    }
+  };
+
+  //TODO 更多的方法正在陆续的加入中
+
+  window.ez = v;
+
+})(window);
